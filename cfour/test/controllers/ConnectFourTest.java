6f47b8b42462a2a7e840e352f2d6cca98fc11031
sahil.shah <sahil.shah@imanage.com>
2019-06-18 09:39:37 -0500

    unit tests

---
+++ cfour/test/controllers/ConnectFourTest.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectFourTest {
+    @Test
+    void name() {
+        assertTrue(true);
+    }
+}
